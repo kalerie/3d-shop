3d177d43a8f9bdf39dd9fcd388a57fa44a44a0da
kalerie <valeriiakuryliak@gmail.com>
2024-12-06 20:29:42 +0100

    applied canvas

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -18,38 +18,98 @@ import {
 const Customizer = () => {
   const snap = useSnapshot(state);
 
-  // const [file, setFile] = useState('');
+  const [file, setFile] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
 
-  // const [prompt, setPrompt] = useState('');
-  // const [generatingImg, setGeneratingImg] = useState(false);
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
+      case "aipicker":
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
+      default:
+        return null;
+    }
+  };
 
-  // const [activeEditorTab, setActiveEditorTab] = useState("");
-  // const [activeFilterTab, setActiveFilterTab] = useState({
-  //   logoShirt: true,
-  //   stylishShirt: false,
-  // })
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("Please enter a prompt");
 
-  // const generateTabContent = () => {
-  //   switch (activeEditorTab) {
-  //     case "colorpicker":
-  //       return <ColorPicker />
-  //     case "filepicker":
-  //       return <FilePicker
-  //         file={file}
-  //         setFile={setFile}
-  //         readFile={readFile}
-  //       />
-  //     case "aipicker":
-  //       return <AIPicker
-  //         prompt={prompt}
-  //         setPrompt={setPrompt}
-  //         generatingImg={generatingImg}
-  //         handleSubmit={handleSubmit}
-  //       />
-  //     default:
-  //       return null;
-  //   }
-  // }
+    try {
+      // setGeneratingImg(true);
+      // const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify({
+      //     prompt,
+      //   })
+      // })
+      // const data = await response.json();
+      // handleDecals(type, `data:image/png;base64,${data.photo}`)
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
+    }
+  };
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if (!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab);
+    }
+  };
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case "logoShirt":
+        state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case "stylishShirt":
+        state.isFullTexture = !activeFilterTab[tabName];
+        break;
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+        break;
+    }
+
+    // after setting the state, activeFilterTab is updated
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName],
+      };
+    });
+  };
+
+  const readFile = (type) => {
+    reader(file).then((result) => {
+      handleDecals(type, result);
+      setActiveEditorTab("");
+    });
+  };
 
   return (
     <AnimatePresence>
@@ -63,8 +123,14 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
                 ))}
+
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
@@ -90,11 +156,9 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => handleActiveFilterTab(tab.name)}
               />
-              // isActiveTab={activeFilterTab[tab.name]}
-              //   handleClick={() => handleActiveFilterTab(tab.name)}
             ))}
           </motion.div>
         </>
